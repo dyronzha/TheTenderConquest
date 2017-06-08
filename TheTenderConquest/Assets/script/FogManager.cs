e85cda2e3e63029f434c6bed8a2c8bef689e10ae
h94ru4vm0 <fm06vul3fu6@gmail.com>
2017-06-08 23:17:39 +0800

    整合
    1.開始畫面按鈕、特效
    2.第一關雪、霧生成
    3.UI更新

---
+++ TheTenderConquest/Assets/script/FogManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FogManager : MonoBehaviour {
+
+    public GameObject fog;
+    public GameObject FogSystem;
+    public GameObject copyfog;
+    bool upperFog;
+    // Use this for initialization
+    void Start () {
+        InvokeRepeating("FogMaker",1.5f,6);
+        upperFog = true;
+    }
+    void FogMaker()
+    {   
+        if (upperFog)
+        {
+            copyfog = Instantiate(fog, new Vector3(0, Random.Range(8f, 20f), 0), Quaternion.identity);
+            copyfog.transform.parent = FogSystem.transform;
+            copyfog.transform.localScale = new Vector3(fog.transform.localScale.x, fog.transform.localScale.y, fog.transform.localScale.z);
+            upperFog = false;
+        }
+        else
+        {
+            copyfog = Instantiate(fog, transform.position, Quaternion.identity);
+            copyfog.transform.parent = FogSystem.transform;
+            copyfog.transform.localScale = new Vector3(fog.transform.localScale.x, fog.transform.localScale.y, fog.transform.localScale.z);
+            upperFog = true;
+        } 
+    }
+    
+}
